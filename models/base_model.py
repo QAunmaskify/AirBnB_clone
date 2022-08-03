5975dc16d432dd189c029a0669bdad8e39c33ace
Jasmine <jasmineolofin@gmail.com>
2022-08-03 11:28:38 +0000

    added base_model module (file). no package on feature_bit branch

---
+++ models/base_model.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""BaseModel Class"""
+
+
+class BaseModel:
+    """
+    Base class, BaseModel, defines all common public
+    instance attributes: id, created_at, updated_at.
+
+    id: assign unique string value when an instance
+    is created.
+
+    created_at: assign datetime value on instance creation.
+
+    updated_at: assign datetime value on instance creation
+    and modify whenever an instance is updated.
+    """
