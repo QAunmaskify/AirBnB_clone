88fbcafca7e02b65ef697eeda85a9b6ddc0c7886
Jasmine <jasmineolofin@gmail.com>
2022-08-05 10:28:48 +0000

    Added new method (FileStorage interface method)

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,13 +4,27 @@
 
 class FileStorage:
     """
-    FileStorage uses file_path and objects attributes as follow:
+    FileStorage uses file_path and objects attributes
+    (abstracted data) as follow:
 
-    file_path (private): keeps a reference to JSON file path.
+    file_path (private): keeps a reference to JSON file
+    path.
 
-    objects (dict): private class attribute to store all instance_obj.
+    objects (dict): private class attribute to store all
+    instance_obj.
     """
 
     __file_path = 'file.json'
     __objects = {}
 
+    def all(self):
+        """
+        Returns all the contents of objects (private class attribute)
+        to any module that requests for it.
+
+        Arg:
+            self (BaseModel object instance): pass in BaseModel instance
+            obj
+        """
+        return FileStorage.__objects
+        """
