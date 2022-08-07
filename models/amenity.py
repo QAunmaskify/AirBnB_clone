4edec0b6347b44b03630b67d6f00d2681c8d3068
Jasmine <jasmineolofin@gmail.com>
2022-08-07 13:32:09 +0000

    commit amenity.py file

---
+++ models/amenity.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Amenity class"""
+
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    '''
+    Amenity class extend BaseModel
+
+    Arg:
+        BaseModel - super class
+    '''
+    name = ''
