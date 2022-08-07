61989b5ec0dfec372ece3c6a33691fe40221385b
Jasmine <jasmineolofin@gmail.com>
2022-08-07 13:12:38 +0000

    State class model added

---
+++ models/state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""State Class"""
+
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    '''
+    State class extends BaseModel to inherit
+
+    Arg:
+        BaseModel - super class
+    '''
+    name = ''
