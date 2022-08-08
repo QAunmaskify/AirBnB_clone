555d5d8aaf15d16a8e07c947bc360a8bd2919bef
Jasmine <jasmineolofin@gmail.com>
2022-08-08 00:52:52 +0000

    Worked on pycodestyle and docstring compliance

--- models/place.py
+++ models/place.py
@@ -7,10 +7,21 @@ from models.base_model import BaseModel
 
 class Place(BaseModel):
     '''
-    Place class extends BaseModel
+    Place class extends BaseModel (super class)
 
-    Arg:
-        BaseModel - super class
+
+    Attributes:
+        city_id (str): stores city unique number, FK.
+        user_id (str): holds user unique number, FK.
+        name (str): name of a place.
+        description (str): information about a place.
+        number_rooms (int): number of rooms in a place.
+        number_bathrooms (int): no. of bathroom of a place.
+        max_guest (int): place capacity.
+        price_by_night (int): price for night services.
+        latitude (float): latitude point.
+        longitude (float): longitudinal point.
+        amenity_ids (:obj: list of :obj: str): stores list of amenity ids
     '''
     city_id = ''
     user_id = ''
@@ -23,4 +34,3 @@ class Place(BaseModel):
     latitude = 0.0
     longitude = 0.0
     amenity_ids = []
-
