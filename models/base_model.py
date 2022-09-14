baf09d33c8381f18aae355a59322e6a6e939773c
Jasmine <jasmineolofin@gmail.com>
2022-09-13 22:48:43 -0700

    Restructure code to use DB storage... process

--- models/base_model.py
+++ models/base_model.py
@@ -6,13 +6,33 @@ from datetime import datetime
 from uuid import uuid4
 from models import storage
 
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import String, Column, DateTime
 
-class BaseModel:
+
+Base = declarative_base()
+
+
+class BaseModel(Base):
     """
     Base class, BaseModel, defines all basic common public
     instance attributes: id, created_at, updated_at which
     are inherited by other class that extends this base class.
     """
+    id = Column(String(60), nullable=False, primary_key=True)
+
+    created_at = Column(
+        DateTime,
+        default=datetime.utcnow,
+        nullable=False
+    )
+
+    updated_at = Column(
+        DateTime,
+        default=datetime.utcnow,
+        nullable=False
+    )
+
     def __init__(self, *args, **kwargs):
         """__init__ initializes BaseModel instance attributes
 
@@ -35,6 +55,8 @@ class BaseModel:
                     setattr(self, key, value)
 
         else:
+            self.name = 'No name'
+
             #: str: id attribute set to str
             self.id = str(uuid4())
 
@@ -43,7 +65,7 @@ class BaseModel:
 
             #: datetime: created_at set to datetime obj
             self.created_at = datetime.now()
-            storage.new(self)
+            # storage.new(self)
 
     def save(self) -> None:
         """
@@ -57,6 +79,7 @@ class BaseModel:
             None either successful or not.
         """
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self) -> dict:
@@ -86,8 +109,11 @@ class BaseModel:
                 new_dict[key] = value.isoformat()
             else:
                 new_dict[key] = value
-            new_dict['__class__'] = self.__class__.__name__
+        new_dict['__class__'] = self.__class__.__name__
         return new_dict
+
+    def delete(self) -> None:
+        storage.delete(self)
 
     def __str__(self) -> str:
         """Return the string representation of the instance"""
